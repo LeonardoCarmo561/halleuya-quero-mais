e3cf4d0395275af41eb6a04a660c07a589018d8e
LeonardoCarmo561 <carmoleo561@gmail.com>
2023-07-19 16:18:24 -0300

    update fonts

--- src/screens/Testimoniasl.tsx
+++ src/screens/Testimoniasl.tsx
@@ -16,14 +16,14 @@ export default function Testimonials({ handlePlaySound, sounds }: TestimonialsPr
           <Pressable
             onPress={() => handlePlaySound(sounds[0])}
             className="flex flex-1 bg-black justify-center items-center px-4">
-            <Text className="text-white font-[FivoSans-Heavy-Oblique] text-center text-[15px]">
+            <Text className="text-white font-[FivoSans-Heavy-Oblique] text-center text-auto">
               Em Deus, encontrei a verdadeira essência de quem eu sou
             </Text>
           </Pressable>
           <Pressable
             onPress={() => handlePlaySound(sounds[1])}
             className="flex flex-[2] bg-black justify-center items-center px-5">
-            <Text className="text-white font-[FivoSans-Heavy-Oblique] text-center text-[15px]">
+            <Text className="text-white font-[FivoSans-Heavy-Oblique] text-center text-auto">
               Tinha tudo neste mundo, mas minha experiência com Deus me lançou a uma vida sobrenatural
             </Text>
           </Pressable>
@@ -32,14 +32,14 @@ export default function Testimonials({ handlePlaySound, sounds }: TestimonialsPr
           <Pressable
             onPress={() => handlePlaySound(sounds[2])}
             className="flex flex-1 bg-black justify-center items-center px-7">
-            <Text className="text-white font-[FivoSans-Heavy-Oblique] text-center text-[15px]">
+            <Text className="text-white font-[FivoSans-Heavy-Oblique] text-center text-auto">
               De uma vida normal de estudos a uma vida do outro mundo em Cristo
             </Text>
           </Pressable>
           <Pressable
             onPress={() => handlePlaySound(sounds[3])}
             className="flex flex-1 bg-black justify-center items-center px-4">
-            <Text className="text-white font-[FivoSans-Heavy-Oblique] text-center text-[15px]">
+            <Text className="text-white font-[FivoSans-Heavy-Oblique] text-center text-auto">
               Obrigado por minha mãe a participar da Crisma, começou uma vida nova
             </Text>
           </Pressable>
@@ -53,14 +53,14 @@ export default function Testimonials({ handlePlaySound, sounds }: TestimonialsPr
           <Pressable
             onPress={() => handlePlaySound(sounds[4])}
             className="flex flex-[2] bg-black justify-center items-center px-5">
-            <Text className="text-white font-[FivoSans-Heavy-Oblique] text-center text-[15px]">
+            <Text className="text-white font-[FivoSans-Heavy-Oblique] text-center text-auto">
               Sou um jovem apaixonado por Deus. Encontrar a Deus é encontrar a Verdade
             </Text>
           </Pressable>
           <Pressable
             onPress={() => handlePlaySound(sounds[5])}
             className="flex flex-1 bg-black justify-center items-center px-2">
-            <Text className="text-white font-[FivoSans-Heavy-Oblique] text-center text-[15px]">Uma alegria transbordante para o mundo</Text>
+            <Text className="text-white font-[FivoSans-Heavy-Oblique] text-center text-auto">Uma alegria transbordante para o mundo</Text>
           </Pressable>
         </View>
         <Pressable
@@ -72,7 +72,7 @@ export default function Testimonials({ handlePlaySound, sounds }: TestimonialsPr
                 items-center
               "
         >
-          <Text className="text-white font-[FivoSans-Heavy-Oblique] text-center text-[15px] p-6">
+          <Text className="text-white font-[FivoSans-Heavy-Oblique] text-center text-auto p-6">
             Numa busca da felicidade nas festas, drogas e no estilo de vida mundano,
             tudo mudou quando experimentei o verdadeiro Amor de Deus
           </Text>
